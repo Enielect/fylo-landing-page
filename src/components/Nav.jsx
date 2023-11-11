b1c8c15e65dccb726facaccd72b90bd316ae2a1f
Enielect <eniolafaithful57@gmail.com>
2023-11-11 23:05:30 +0100

    altered the nav to support mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,40 @@
 import { useState } from "react";
+import { HiBars3BottomLeft, HiOutlineXMark } from "react-icons/hi2";
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+
   const initial = "flex  items-center justify-between";
   return (
-    <div className=" grid grid-cols-[130px_1fr] py-5 gap-8 text-white items-center px-10 lg:flex lg:justify-between">
+    <div className="relative grid grid-cols-[130px_1fr] py-5 gap-8 text-white items-center px-10 sp:flex sp:justify-between">
       <img src="./images/logo.svg" alt="" className="h-10 w-32" />
 
-      <div className="flex justify-between lg:gap-8">
+      <button
+        onClick={() => setIsOpen(true)}
+        className="absolute right-10 sp:hidden"
+      >
+        <HiBars3BottomLeft style={{ fontSize: "3rem" }} />
+      </button>
+
+      <div
+        className={`h-screen absolute  ${
+          isOpen ? "w-screen" : "w-0 left-[-44px]"
+        } transition-all duration-300 text-black font-bold top-[0px] text-xl  backdrop-blur-sm`}
+      >
+        <button
+          className="absolute right-10 top-4"
+          onClick={() => setIsOpen(false)}
+        >
+          <HiOutlineXMark style={{ fontSize: "3rem" }} />
+        </button>
+        <div className={`flex flex-col  gap-11 items-center mt-[100px]`}>
+          <div >Features</div>
+          <div>Team</div>
+          <div>Sign In</div>
+        </div>
+      </div>
+      <div className="sp:flex sp:gap-6 hidden">
         <div>Features</div>
         <div>Team</div>
         <div>Sign In</div>
